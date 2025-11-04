9bc906f370e84c99a572906d4f2d5ba20801ccc5
rls-dnt <ramses.lopez@dentons.com>
2025-11-04 17:24:26 -0600

    ready for publish

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,9 @@ module.exports = {
       max_restarts: 10,
       restart_delay: 5000,
       env: {
-        NODE_ENV: 'production',
-        RESTART_ON_ERROR: 'true', // 'false' para mantener vivo el scheduler aunque haya errores
-        ONE_SHOT: 'false', // 'true' para ejecutar una vez y salir
-        TZ: 'America/Costa_Rica'
-      }
+        NODE_ENV: 'production'
+      },
+      env_file: '.env' // PM2 cargará las variables desde este archivo
     }
   ]
 };
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 ### Start app in server
 
     pm2 start ecosystem.config.js --env production
+
+    pm2 start ecosystem.config.js
+
+    pm2 logs athena-snapshot
+
+    pm2 restart athena-snapshot
